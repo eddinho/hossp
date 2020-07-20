c0350f73399bed7d38edf7001d32539f0b8da058
Eddinho <7454690+eddinho@users.noreply.github.com>
2020-07-20 19:10:26 +0200

    thirdparty[D[D[D[D[D[D[D[D[D[D[3~[3~[3~[3~[3~[3~[3~[3~[3~[C[C[C[C[C[C[C[C[Ce: move rv2d and adjust build file

--- thirdparty/rv2d/rv2d.h
+++ thirdparty/rv2d/rv2d.h
@@ -1,5 +1,5 @@
 /*
-Copyright (c) 2020 seb
+Copyright (c) 2020 S.E. B.
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
